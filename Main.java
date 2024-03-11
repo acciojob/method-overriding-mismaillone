0c7cd3f3b6215d8b57c7e9294ed781eba3ed1f44
mismaillone <warlockmachine34@gmail.com>
2024-03-11 04:13:15 -0400

    Assignment Completed

--- Main.java
+++ Main.java
@@ -1,31 +1,30 @@
 package com.driver;
 
+package com.driver;
+
 public class Main {
 
-   public static class A {
+    public static class A {
         public String meth() {
-
             return "Hello World";
         }
-
     }
 
     public static class B extends A {
-            @Override
-      public String meth() {
-        return "Method is Overriddden";
-      } 
+        @Override
+        public String meth() {
+            return "Method is Overridden";
+        }
     }
 
     public static void main(String[] args) {
         A obj = new A();
-
         B obj1 = new B();
 
-       String s =  obj.meth();
-       System.out.println(s);
+        String s = obj.meth();
+        System.out.println(s);
 
-       String c = obj1.meth();
-       System.out.println(c);
+        String c = obj1.meth();
+        System.out.println(c);
     }
 }
